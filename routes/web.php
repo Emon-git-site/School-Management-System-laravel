e62d9192f2a3dcf502e9d5eb50603d92e5494402
Emon-git-site <136096636+Emon-git-site@users.noreply.github.com>
2024-02-29 09:48:14 +0600

    t6 done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [AuthController::class, 'login']);
+Route::post('/', [AuthController::class, 'AuthLogin']);
 
 Route::get('admin/dashboard', function () {
     return view('admin.dashboard');
